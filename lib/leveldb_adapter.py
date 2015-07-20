99e0b8600da6d79c4536de1c8e4277aaa0c21816
Marc Bickel <marc.bickel@mytum.de>
2015-07-20 11:27:30 +0200

    Initial Commit

---
+++ lib/leveldb_adapter.py
@@ -0,0 +1,253 @@
+__author__ = 'mbickel'
+
+import numpy as np
+import re
+import io
+import os
+
+import plyvel
+import caffe
+
+# test
+#import nifti_helper as nh
+
+class Key:
+    def __init__(self):
+        self.__regex = '(^)(\d){8}_(\d){5}_(seg|img){1}_(xy|xz|yz){1}_(\d){4}_(\d){2}($)'
+
+        self.counter = None
+        self.seg_uid = None
+        self.type = None
+        self.slice_type = None
+        self.slice_count = None
+
+        self.slice_mod = None
+
+        self.key = None
+
+    def get_counter(self):
+        return self.counter
+
+    def set_counter(self, counter):
+        if counter < 0 or counter > 99999999:
+            raise ValueError("Counter must be between 0 and 99,999,999")
+        self.counter = counter
+
+    def get_seg_uid(self):
+        return self.seg_uid
+
+    def set_seg_uid(self, seg_uid):
+        if seg_uid < 0 or seg_uid > 99999:
+            raise ValueError("Seg_uid must be between 0 and 99,999")
+        self.seg_uid = seg_uid
+
+    def get_type(self):
+        return self.type
+
+    def set_type(self, type):
+        if type == 'seg' or type == 'img':
+            self.type = type
+        else:
+            raise ValueError("Type must be img or seg.")
+
+    def get_slice_type(self):
+        return self.slice_type
+
+    def set_slice_type(self, slice_type):
+        if slice_type == 'xy' or slice_type == 'xz' or slice_type == 'yz':
+            self.slice_type = slice_type
+        else:
+            raise ValueError("slice_type must be xy, xz or yz")
+
+    def get_slice_count(self):
+        return self.slice_count
+
+    def set_slice_count(self, slice_count):
+        if slice_count < 0 or slice_count > 9999:
+            raise ValueError("Slice_count must be between 0 and 9,999 (" + str(slice_count) + ")")
+        self.slice_count = slice_count
+
+    def get_slice_mod(self):
+        return self.slice_mod
+
+    def set_slice_mod(self, slice_mod):
+        if slice_mod < 0 or slice_mod > 99:
+            raise ValueError("Slice_mod must be between 0 and 99")
+        self.slice_mod = slice_mod
+
+    def get_key(self):
+        self.create_key()
+        return self.key
+
+    def set_key(self, key):
+        if re.match(self.__regex, key) == None:
+            raise ValueError('Wrong key: ' + key)
+        else:
+            self.key = key
+
+    def create_key(self):
+        if self.counter == None or self.seg_uid == None or self.type == None or self.slice_type == None or self.slice_count == None:
+            raise ValueError("One of the required values is not set.")
+        else:
+            self.set_key('%08d_%05d_%s_%s_%04d_%02d' % (self.counter, self.seg_uid, self.type, self.slice_type, self.slice_count, self.slice_mod))
+
+    def disassemble_key(self):
+        if self.key == None:
+            raise ValueError("Key is not set.")
+        else:
+            key_parts = self.key.split('_')
+            self.counter = int(key_parts[0])
+            self.seg_uid = int(key_parts[1])
+            self.type = key_parts[2]
+            self.slice_type = key_parts[3]
+            self.slice_count = int(key_parts[4])
+            self.slice_mod = int(key_parts[5])
+
+
+class ImageAdapter:
+    def __init__(self, db_path):
+        self.db = plyvel.DB(os.path.normpath(db_path), create_if_missing=True, write_buffer_size=268435456)
+        self.wb = None
+        self.batch_counter = 0
+
+    def __del__(self):
+        self.db.close()
+
+    def add_batch(self, key, ser_img, x_dim, y_dim):
+        if self.wb == None:
+            self.wb = self.db.write_batch()
+
+        # first create key
+        key.create_key()
+
+        # now create datum
+        datum = caffe.proto.caffe_pb2.Datum()
+        datum.height = y_dim
+        datum.width = x_dim
+        datum.channels = 1
+        datum.data = ser_img
+
+        # add to batch
+        self.wb.put(bytes(key.get_key()), bytes(datum.SerializeToString()))
+
+        # auto write
+        self.batch_counter += 1
+        if self.batch_counter == 1000:
+            self.write()
+
+    def write(self):
+        if self.wb == None:
+            return
+
+        self.wb.write()
+        self.wb = None
+        self.batch_counter = 0
+
+    def read_img(self, key):
+        key.create_key()
+        raw_datum = self.db.get(bytes(key.get_key()))
+
+        datum = caffe.proto.caffe_pb2.Datum()
+        datum.ParseFromString(raw_datum)
+
+        flat_img = np.fromstring(datum.data, dtype=np.uint8)
+        img = flat_img.reshape(datum.height, datum.width)
+
+        return img
+
+    def dump_img(self, key, path):
+        img = self.read_img(key)
+
+        with io.open(os.path.normpath(path + '/' + key.get_key() + '.ppm'), 'wb') as ppm:
+            ppm.write(b'P5\n' + bytes(img.shape[0]) + b' ' + bytes(img.shape[1]) + b'\n' + b'255\n')
+            ppm.write(img.tostring())
+
+    def read_volume(self, key):
+        slices = []
+        vol = None
+
+        # first try to get the first slice
+        try:
+            key.set_slice_count(0)
+            slices.append(self.read_img(key))
+        except:
+            raise ValueError("Can't find starting slice: " + key.get_key())
+
+        # now iterate over the slices until the end is reached
+        i = 0
+        while True:
+            i += 1
+            try:
+                key.set_slice_count(i)
+                slices.append(self.read_img(key))
+            except:
+                break
+
+        # add up slices to volume
+        vol = np.dstack(tuple(slices))
+
+        if key.get_slice_type() == 'xy':
+            return vol
+        elif key.get_slice_type() == 'xz':
+            vol = np.swapaxes(vol, 1, 2)
+            return vol
+        else:
+            vol = np.swapaxes(vol, 0, 2)
+            vol = np.swapaxes(vol, 1, 2)
+            return vol
+#
+#
+# def main():
+#     # # open db
+#     # db = plyvel.DB('/tmp/testdb/', create_if_missing=True, write_buffer_size=268435456)
+#     #
+#     # # add some data
+#     # db.put(b'key', b'value')
+#     # db.put(b'another-key', b'another-value')
+#     #
+#     # # print data
+#     # print(db.get(b'key'))
+#     #
+#     # # close db
+#     # db.close()
+#
+#     key = Key()
+#     key.set_key('00045678_12345_img_yz_1234_00')
+#     key.disassemble_key()
+#     key.create_key()
+#
+#     print(key.get_key())
+#     #print(key.slice_count)
+#
+#     ia = ImageAdapter('/media/ramdisk/test_img')
+#
+#     # # prepare slice
+#     # nh = NiftiDBHelper('/media/nas/sqlitedb/processed/processedFinalOrderedSegmented.sql')
+#     # nh.q_random()
+#     # for i, slc in enumerate(yz_slices(nh.get_ct_img())):
+#     #     key.set_slice_count(i)
+#     #     arr = hounsfield_to_byte(slc)
+#     #     val = serialize_slice(arr)
+#     #     ia.add_batch(key, val, arr.shape[1], arr.shape[0])
+#     #
+#     # ia.write()
+#
+#     # read
+#     key.set_slice_count(250)
+#     img = ia.read_img(key)
+#
+#     nh.show_slices([img, img])
+#     nh.plt.show()
+#
+#     ia.dump_img(key, '/media/ramdisk')
+#
+#     # read vol
+#     key.set_slice_count(60)
+#     vol = ia.read_volume(key)
+#
+#     nh.show_slices([vol[:,:,320], vol[:,300,:], vol[300, : , :]])
+#     nh.plt.show()
+#
+#
+# if __name__ == '__main__':
+#     main()
---
+++ lib/nifti_helper.py
@@ -0,0 +1,409 @@
+__author__ = 'mbickel'
+
+import nibabel as nib
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+import os
+import sys
+import random
+
+import imp
+f3 = imp.load_source('f3', os.path.normpath('../lib/fire3db.py'))
+
+##
+#
+# Methods requiring initialization of database.
+#
+##
+
+class NiftiDBHelper:
+    """Helper class to get niftis or numpy tensors registered in fire3 SQLite database."""
+
+    def __init__(self, path):
+        self.set_db(path)
+
+        self.count = self.q_total()
+        self.row = None
+
+    def __del__(self):
+        f3.close_db()
+
+    def set_db(self, path):
+        """
+        Set a sqlite db for this package.
+        :param path: Path to sqlite db with segmented table (see fire3db package)
+        """
+        f3.init_db(os.path.normpath(path))
+
+    def get_row(self):
+        if self.row != None:
+            return self.row
+
+    def q_first_row(self):
+        self.row = f3.Segmented.select().first()
+        return self.row
+
+    def q_x_row(self, nb):
+        """Query Xth row in Segmented table. Start with 0"""
+        if nb > (self.q_total() - 1):
+            raise IndexError
+
+        self.row = f3.Segmented.select().offset(nb).limit(1).first()
+        return self.row
+
+    def q_total(self):
+        """Get the count of rows in the Segmented table."""
+        return f3.Segmented.select().count()
+
+    def q_random(self):
+        """Query a random row"""
+        self.row = self.q_x_row(random.randint(0, (self.q_total() - 1)))
+        return self.row
+
+    def q_all_rows(self):
+        """Lazy iterator over all rows in Segmented table.
+        :return:Lazy segmented iterator.
+        """
+        return f3.Segmented.select().iterator()
+
+    def get_current_row(self):
+        """Get the currently cached row or None if no query was done yet."""
+        return self.row
+
+    def get_ct_img(self):
+        """Get currently cached CT-Image"""
+        return load_nifti(self.row.ctNiftiFilename)
+
+    def get_ct_img_data(self):
+        """Get image data of currently cached CT-Image as numpy tensor"""
+        return get_img_data(self.get_ct_img())
+
+    def get_ct_img_dim(self):
+        """
+        Get dimension of currently cached CT-Image
+        :return: x range, y range, z range (3 return vals)
+        """
+        return get_dim(self.get_ct_img())
+
+    def get_seg_img(self):
+        """Get currently cached Segmentation"""
+        return load_nifti(self.row.segNiftiFilename)
+
+    def get_seg_img_data(self):
+        """Get numpy tensor of currently cached segmentation tensor"""
+        return get_img_data(self.get_seg_img())
+
+    def get_seg_img_dim(self):
+        """
+        Get dimension of currently cached Segmentation
+        :return: x range, y range, z range (3 return vals)
+        """
+        return get_dim(self.get_seg_img())
+
+
+class Segmentations:
+    """Iterator for looping over all segmented niftis in the database."""
+
+    def __init__(self, path):
+        self.data = NiftiDBHelper(path)
+        self.index = 0
+        self.total = self.data.q_total()
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        if self.index == self.total:
+            raise StopIteration
+        else:
+            self.data.q_x_row(self.index)
+            self.index += 1
+            return self.data
+
+
+##
+#
+# Stateless functions.
+#
+##
+
+def load_nifti(path):
+    """
+    Load a nifti by its file path.
+    :param path: Path to a nifti file.
+    :return: nibabel nifti wrapper obj
+    """
+    return nib.load(os.path.normpath(path))
+
+
+def show_slices(slices):
+    """ Function to display row of image slices """
+    fig, axes = plt.subplots(1, len(slices))
+    for i, slice in enumerate(slices):
+        if len(slices) == 1:
+            axes.imshow(slice, cmap="gray")
+        else:
+            axes[i].imshow(slice, cmap="gray")
+
+
+def get_dim(img):
+    """
+    Get dimension of img.
+    :param img: Nibabel nifti wrapper.
+    :return: x range, y range, z range (3 return vals)
+    """
+    if len(img.shape) == 3:
+        return img.shape[0], img.shape[1], img.shape[2]
+    else:
+        sys.stderr.write('Error, CT-Image must have 3 dimensions.')
+        raise Exception
+
+
+def get_img_data(img):
+    """Get image data of img as numpy tensor"""
+    return img.get_data()
+
+
+def plot_at_center(img):
+    """Takes a nibabel nifti wrapper and prints out three centered slices"""
+    x, y, z = get_dim(img)
+
+    x_center = round((x - 1) / 2)
+    y_center = round((y - 1) / 2)
+    z_center = round((z - 1) / 2)
+
+    dat = get_img_data(img)
+
+    show_slices([dat[:, :, z_center], dat[:, y_center, :], dat[x_center, :, :]])
+    plt.suptitle("Centered slices for image (xy, xz, yz)")
+    plt.show()
+
+
+def plot_at(x, y, z, img):
+    """Takes a nibabel nifti wrapper and prints out three slices a (x, y, z)"""
+    x_max, y_max, z_max = get_dim(img)
+
+    if (x > x_max - 1) or (y > y_max - 1) or (z > z_max - 1):
+        raise IndexError
+    else:
+        dat = get_img_data(img)
+
+        show_slices([dat[:, :, z], dat[:, y, :], dat[x, :, :]])
+        plt.suptitle("Slices centered at (" + str(x) + ", " + str(y) + ", " + str(z) + "). (xy, xz, yz)")
+        plt.show()
+
+
+def xy_slices(img):
+    """Takes a nibabel nifti wrapper and returns a iterator over all xy slices"""
+    _, _, z_max = get_dim(img)
+
+    dat = get_img_data(img)
+
+    for i in xrange(z_max - 1):
+        yield dat[:, :, i]
+
+
+def xz_slices(img):
+    """Takes a nibabel nifti wrapper and returns a iterator over all xz slices"""
+    _, y_max, _ = get_dim(img)
+
+    dat = get_img_data(img)
+
+    for i in xrange(y_max - 1):
+        yield dat[:, i, :]
+
+
+def yz_slices(img):
+    """Takes a nibabel nifti wrapper and returns a iterator over all yz slices"""
+    x_max, _, _ = get_dim(img)
+
+    dat = get_img_data(img)
+
+    for i in xrange(x_max - 1):
+        yield dat[i, :, :]
+
+
+def serialize_slice(slc):
+    """Serialize numpy matrix to byte stream."""
+    if np.amax(slc) > 255:
+        raise ValueError("Slice must be a uint8 byte array.")
+    else:
+        # np.rot90(slc)...
+        return np.rot90(slc).tostring()
+
+
+def hounsfield_to_byte_dyn(arr, c_min=0.1, c_max=0.3):
+    """
+    Numpy array with hounsfield units will be clipped for contrast enhancement and then normalized to np.uint8.
+    Standard clipping is set to favor contrast enhanced tissue. If you want to have a higher bone resolution, set c_max at least to 0.8
+    """
+
+    # calc min and max
+    min = np.amin(arr)
+    max = np.amax(arr)
+
+    if min <= 0:
+        # clip to c_min and c_max
+        c_arr = np.clip(arr, min * c_min, max * c_max)
+
+        # right shift to zero
+        slc_0 = np.add(np.abs(min * c_min), c_arr)
+    else:
+        # clip to c_max
+        c_arr = np.clip(arr, min, max * c_max)
+
+        # left shift to zero
+        slc_0 = np.subtract(c_arr, min)
+
+    # normalization
+    norm_fac = np.amax(slc_0)
+    if norm_fac != 0:
+        norm = np.divide(
+            np.multiply(
+                slc_0,
+                255
+            ),
+            np.amax(slc_0)
+        )
+    else:  # don't divide through 0
+        norm = np.multiply(slc_0, 255)
+
+    return np.array(norm, dtype=np.uint8)
+
+
+def hounsfield_to_byte_stat(arr, c_min=-150, c_max=300):
+    min = np.amin(arr)
+
+    if min <= 0:
+        # clip
+        c_arr = np.clip(arr, c_min, c_max)
+
+        # right shift to zero
+        slc_0 = np.add(np.abs(min), c_arr)
+    else:
+        # clip
+        c_arr = np.clip(arr, c_min, c_max)
+
+        # left shift to zero
+        slc_0 = np.subtract(c_arr, min)
+
+    # normalization
+    norm_fac = np.amax(slc_0)
+    if norm_fac != 0:
+        norm = np.divide(
+            np.multiply(
+                slc_0,
+                255
+            ),
+            np.amax(slc_0)
+        )
+    else:  # don't divide through 0
+        norm = np.multiply(slc_0, 255)
+
+    return np.array(norm, dtype=np.uint8)
+
+
+def main():
+    # ### For test purposes only
+    # f3.init_db('/media/nas/sqlitedb/processed/processedFinalOrderedSegmented.sql')
+    #
+    # #ct_img = nib.load(f3.Segmented.select().first().ctNiftiFilename)
+    # ct_img = nib.load('/media/nas/niftis_segmented/CRF_174/2013-12-06/LightSpeed_Pro_32/segmented/368984_liv_x.nii')
+    # ct_img_dat = ct_img.get_data()
+    #
+    # print(ct_img.shape)
+    #
+    # # Now plot something
+    # slice_0 = ct_img_dat[300, :, :]
+    # slice_1 = ct_img_dat[:, 300, :]
+    # slice_2 = ct_img_dat[:, :, 520]
+    # show_slices([slice_0, slice_1, slice_2])
+    # plt.suptitle("Center slices for EPI image")
+    # plt.show()
+    #
+    #
+    # ### Close DB
+    # f3.close_db()
+
+    # ct_img = nib.load('/media/nas/niftis_segmented/CRF_174/2013-12-06/LightSpeed_Pro_32/segmented/368984.nii')
+    # ct_img = nib.load('/media/nas/niftis_segmented/CRF_036/2008-05-09/Emotion_Duo/segmented/03040001.nii')
+    # plot_at(150, 350, 550, ct_img)
+
+    nh = NiftiDBHelper('/media/nas/sqlitedb/processed/finalDBNew.sql')
+    nh.q_random()
+    ct_img = nh.get_ct_img()
+    ct_seg = nh.get_seg_img()
+
+    for i, slc in enumerate(xy_slices(ct_img)):
+
+        sol = hounsfield_to_byte_dyn(slc)
+
+        if (i > 1) and (i % 50 == 0):
+            # plot seg
+            for j, slc_seg in enumerate(xy_slices(ct_seg)):
+                if i == j:
+                    show_slices([sol, slc_seg])
+                    plt.show()
+                    break
+
+        print('-')
+        print(np.amin(sol))
+        print(np.amax(sol))
+        print(np.amin(slc))
+        print(np.amax(slc))
+        #
+        # nh = NiftiDBHelper('/media/nas/sqlitedb/processed/processedFinalOrderedSegmented.sql')
+        # for i in range(3):
+        #     nh.q_random()
+        #     plot_at_center(nh.get_ct_img())
+        #
+        # for dat in Segmentations('/media/nas/sqlitedb/processed/processedFinalOrderedSegmented.sql'):
+        #     print(dat.get_ct_img_dim())
+        #     print(dat.get_seg_img_dim())
+
+        # print('Hallo Welt!')
+
+        # total = 0
+        # for dat in Segmentations('/media/nas/sqlitedb/processed/processedFinalOrderedSegmented.sql'):
+        #     print(dat.get_ct_img_dim())
+        #     _, _, z = dat.get_ct_img_dim()
+        #     total += z
+        #
+        # print total
+
+    tot_0 = 0.0
+    for i in xy_slices(ct_img):
+        i = hounsfield_to_byte_dyn(i)
+
+        if np.amax(i) < 5:
+            tot_0 += 1
+
+    print('tot blank: ' + str(tot_0))
+
+    ti = 0
+    li = 0
+    le = 0
+    tot = 0
+    for s in xy_slices(ct_seg):
+        tot +=  1
+
+        s = np.clip(s, 0, 2)
+
+        if np.amax(s) == 2:
+            le += 1
+
+        if np.amax(s) == 1:
+            li += 1
+
+        if np.amax(s) == 0:
+            ti += 1
+
+    print('(total, tissue, liver, lesion)')
+    print(str(float(tot)) + ', ' + str(float(float(ti)/float(tot))*100) + ', ' + str(float(float(li)/float(tot))*100) + ', ' + str(float(float(le)/float(tot))*100))
+
+
+
+if __name__ == '__main__':
+    main()
