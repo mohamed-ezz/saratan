10ee28a6d1aaf2f9823c6e4fd7e1009cd570eaa8
Marc Bickel <marc.bickel@mytum.de>
2015-08-21 14:37:32 +0200

    merge with feature/home

---
+++ data/junge.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+python prep_caffe_ds.py /home/mbickel/master/tmp/float/lesion-only-aug10-400-float/lesion-only-aug10-400-float.yaml
+
+python prep_caffe_ds.py /home/mbickel/master/tmp/float/lesion-only-aug10-512-float/lesion-only-aug10-512-float.yaml
